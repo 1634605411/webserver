434c229cb2ba66a082f73d9e926996b61a1de71b
Jiangnan <1634605411@qq.com>
2023-08-25 06:06:00 +0800

    nowcode

---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include "locker.h"
+#include "threadpool.h"
+#include <signal.h>
+#include "http_conn.h"
+
+#define MAX_FD 65535
+#define MAX_EVENT_NUMBER 10000
+
+// 添加信号捕捉
+void addsig(int sig, void(handler)(int))
+{
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;
+    sigfillset(&sa.sa_mask);
+    sigaction(sig, &sa, NULL);
+}
+
+extern void addfd(int epollfd, int fd, bool oneshot);
+
+extern void removefd(int epollfd, int fd);
+
+extern void modfd(int epollfd, int fd, int ev);
+
+int main(int argc, char *argv[])
+{
+
+    if (argc <= 1)
+    {
+        printf("exe:%s port\n", basename(argv[0]));
+        exit(-1);
+    }
+
+    // 获取端口号
+    int port = atoi(argv[1]);
+
+    // 处理信号
+    addsig(SIGPIPE, SIG_IGN);
+    
+    // 创建线程池
+    threadpool<http_conn> *pool = NULL;
+    try
+    {
+        pool = new threadpool<http_conn>;
+    }
+    catch (...)
+    {
+        exit(-1);
+    }
+    // 客户端信息
+    http_conn *users = new http_conn[MAX_FD];
+    
+    // 创建监听的套接字
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+
+    // 端口复用
+    int reuse = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    // 绑定
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+    bind(listenfd, (struct sockaddr *)&address, sizeof(address));
+
+    // 监听
+    listen(listenfd, 5);
+
+    // 创建epoll
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
+
+    // 添加到epoll
+    addfd(epollfd, listenfd, false);
+    http_conn::m_epollfd = epollfd;
+    while (true)
+    {
+        int num = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if (num < 0 && errno != EINTR)
+        {
+            printf("epoll failure");
+            break;
+        }
+
+        // 循环遍历
+        for (int i = 0; i < num; i++)
+        {
+            int sockfd = events[i].data.fd;
+            if (sockfd == listenfd)
+            {
+                // 客户端连接
+                struct sockaddr_in client_address;
+                socklen_t client_addrlen = sizeof(client_address);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlen);
+                if (http_conn::m_user_count >= MAX_FD)
+                {
+                    close(connfd);
+                    continue;
+                }
+                users[connfd].init(connfd, client_address);
+            }
+            else if (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR))
+            {
+                // 异常断开
+                users[sockfd].close_conn();
+            }
+            else if (events[i].events & EPOLLIN)
+            {
+                if (users[sockfd].read())
+                {
+                    pool->append(users + sockfd);
+                }
+                else
+                {
+                    users[sockfd].close_conn();
+                }
+            }
+            else if (events[i].events & EPOLLOUT)
+            {
+                if (!users[sockfd].write())
+                {
+                    users[sockfd].close_conn();
+                }
+            }
+        }
+    }
+    close(epollfd);
+    close(listenfd);
+    delete[] users;
+    delete pool;
+
+    return 0;
+}
